4759b9076ef6e4330eecf6db164e8382ab0c0e70
Muhammad Usman Baig <154676421+UsmanBaig001@users.noreply.github.com>
2024-12-18 19:37:45 +0500

    first commit

---
+++ src/script.js
@@ -0,0 +1,155 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const textureLoader = new THREE.TextureLoader();
+
+// const doorColorTexture = textureLoader.load("/textures/door/color.jpg");
+// const doorAlphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+// const doorAmbientOcclusionTexture = textureLoader.load(
+//   "/textures/door/ambientOcclusion.jpg"
+// );
+// const doorHeightTexture = textureLoader.load("/textures/door/height.jpg");
+// const doorNormalTexture = textureLoader.load("/textures/door/normal.jpg");
+// const doorMetalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+// const doorRoughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+const matcapTexture = textureLoader.load("/textures/matcaps/3.png");
+// const gradientTexture = textureLoader.load("/textures/gradients/5.jpg");
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const light = new THREE.PointLight(0xffffff, 0.5);
+light.position.x = 2;
+light.position.y = 3;
+light.position.z = 4;
+scene.add(light);
+
+/**
+ * Objects
+ */
+
+const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture });
+material.metalness = 0.7;
+material.roughness = 0.2;
+material.side = THREE.DoubleSide;
+
+const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 64, 64), material);
+sphere.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(sphere.geometry.attributes.uv.array, 2)
+);
+sphere.position.x = -1.5;
+const box = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), material);
+box.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(box.geometry.attributes.uv.array, 2)
+);
+const plane = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10, 100, 100),
+  material
+);
+plane.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2)
+);
+plane.position.y = -1.5;
+plane.rotation.x = Math.PI * 0.5;
+
+const torus = new THREE.Mesh(
+  new THREE.TorusGeometry(0.3, 0.2, 64, 128),
+  material
+);
+torus.geometry.setAttribute(
+  "uv2",
+  new THREE.BufferAttribute(torus.geometry.attributes.uv.array, 2)
+);
+torus.position.x = 1.5;
+scene.add(sphere, plane, torus, box);
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 2;
+camera.position.z = 4;
+camera.lookAt(box);
+scene.add(sphere);
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock();
+
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update objects
+  sphere.rotation.y = 0.1 * elapsedTime;
+  box.rotation.y = 0.1 * elapsedTime;
+  torus.rotation.y = 0.1 * elapsedTime;
+
+  sphere.rotation.x = 0.15 * elapsedTime;
+  box.rotation.x = 0.15 * elapsedTime;
+  torus.rotation.x = 0.15 * elapsedTime;
+
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
