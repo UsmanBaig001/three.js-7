9a3d80972b17c6dd891cd53f5e472156bd6a6011
UsmanBaig001 <usmanbaig1572@gmail.com>
2024-12-19 00:27:51 +0500

    feat: rectAreaLight and spotlight

--- src/script.js
+++ src/script.js
@@ -27,26 +27,40 @@ const scene = new THREE.Scene();
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
-scene.add(ambientLight);
-gui.add(ambientLight, "intensity").min(0).max(1).step(0.01);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// scene.add(ambientLight);
+// gui.add(ambientLight, "intensity").min(0).max(1).step(0.01);
 
-const directionalLight = new THREE.DirectionalLight(0x0fffc, 0.5);
-directionalLight.position.set(2, 3, 4);
-scene.add(directionalLight);
-gui.add(directionalLight, "intensity").min(0).max(1).step(0.01);
-gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
+// const directionalLight = new THREE.DirectionalLight(0x0fffc, 0.5);
+// directionalLight.position.set(2, 3, 4);
+// scene.add(directionalLight);
+// gui.add(directionalLight, "intensity").min(0).max(1).step(0.01);
+// gui.add(directionalLight.position, "x").min(-5).max(5).step(0.001);
 
-const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
-scene.add(hemisphereLight);
-gui.add(hemisphereLight, "intensity").min(0).max(1).step(0.01);
+// const hemisphereLight = new THREE.HemisphereLight(0xff0000, 0x0000ff, 0.3);
+// scene.add(hemisphereLight);
+// gui.add(hemisphereLight, "intensity").min(0).max(1).step(0.01);
 
-const pointLight = new THREE.PointLight(0xff9000, 0.5, 10, 2);
-pointLight.position.set(1, 1, 1);
-scene.add(pointLight);
-gui.add(pointLight, "intensity").min(0).max(1).step(0.01);
-gui.add(pointLight.position, "x").min(-5).max(5).step(0.001);
+// const pointLight = new THREE.PointLight(0xff9000, 0.5, 1, 0);
+// pointLight.position.set(1, -0.5, 1);
+// scene.add(pointLight);
+// gui.add(pointLight, "intensity").min(0).max(1).step(0.01);
+// gui.add(pointLight.position, "x").min(-5).max(5).step(0.001);
 
+const rectAreaLight = new THREE.RectAreaLight(0x4e00ff, 2, 1, 2);
+rectAreaLight.position.set(-1.5, 0, -1.5);
+rectAreaLight.lookAt(new THREE.Vector3());
+scene.add(rectAreaLight);
+const spotLight = new THREE.SpotLight(
+  0x78ff00,
+  0.5,
+  10,
+  Math.PI * 0.1,
+  0.25,
+  1
+);
+spotLight.position.set(0, 2, 3);
+scene.add(spotLight);
 // const light = new THREE.PointLight(0xffffff, 0.5);
 // light.position.x = 2;
 // light.position.y = 3;
